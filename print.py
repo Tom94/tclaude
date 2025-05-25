3d6c4ec7ed5cd5a1b69008c4696911a153346bf3
Thomas Müller <git@tom94.net>
2025-05-25 10:38:00 +0200

    feat: print abnormal stop reasons

--- print.py
+++ print.py
@@ -16,100 +16,116 @@ def to_superscript(text: str | int) -> str:
     return text.translate(superscript_map)
 
 
+def write_system_message(message: dict, io: StringIO):
+    io.write("\n# System prompt\n")
+    for content_block in message["content"]:
+        if content_block.type == "text":
+            io.write(f"{content_block.text}\n")
+
+
+def write_user_message(message: dict, io: StringIO, pretty: bool):
+    prompt = f"{common.CHEVRON} "
+    input = message["content"][0]["text"]
+    if pretty:
+        prompt = common.prompt_style(prompt)
+        input = common.input_style(input)
+
+    io.write(f"{prompt}{input}\n")
+
+
+def write_assistant_message(message: dict, io: StringIO):
+    references = {}
+
+    block_type = None
+    for content_block in message["content"]:
+        if block_type is not None and block_type != content_block["type"]:
+            io.write(f"\n\n")
+
+        block_type = content_block.get("type")
+
+        if block_type == "thinking":
+            io.write(f"```thinking\n{content_block['thinking']}\n```")
+        elif block_type == "text":
+            io.write(f"{content_block['text']}")
+        elif block_type == "server_tool_use":
+            name = content_block.get("name")
+            if name == "web_search":
+                io.write(f"Searching the web for `{content_block.get("input", {}).get("query", "<unknown>")}`.")
+            elif name == "code_execution":
+                # It's always Python, see https://docs.anthropic.com/en/docs/agents-and-tools/tool-use/code-execution-tool
+                io.write(f"Running the following Python code:\n\n")
+                io.write(f"```python\n{content_block.get("input", {}).get("code", "<unknown>")}\n```")
+            else:
+                io.write(f"Unknown tool: {name}")
+        elif block_type == "web_search_tool_result":
+            io.write(f"Found {len(content_block.get("content", []))} results.")
+        elif block_type == "code_execution_tool_result":
+            output = content_block.get("content", {})
+
+            if output:
+                return_code = output.get("return_code", 0)
+                if return_code != 0:
+                    io.write(f"Code execution failed with return code {return_code}.")
+
+                stdout = output.get("stdout", "")
+                if stdout:
+                    io.write(f"```stdout\n{stdout.strip()}\n```")
+
+                stderr = output.get("stderr", "")
+                if stderr:
+                    io.write(f"```stderr\n{stderr.strip()}\n```")
+
+        else:
+            io.write(f"```{block_type}\n{content_block}\n```")
+
+        if content_block and content_block.get("type") == "text" and content_block.get("citations"):
+            superscripts = set()
+            for citation in content_block["citations"]:
+                if citation.get("type") != "web_search_result_location":
+                    continue
+                url = citation.get("url")
+                if url is None:
+                    continue
+
+                if not url in references:
+                    references[url] = {
+                        "title": citation.get("title", ""),
+                        "cited_texts": set(),
+                        "id": len(references) + 1,
+                    }
+
+                reference = references[url]
+                if "cited_text" in citation:
+                    reference["cited_texts"].add(citation["cited_text"])
+
+                superscripts.add(str(reference["id"]))
+
+            io.write(f"{to_superscript(','.join(sorted(superscripts)))}")
+
+    if block_type is not None:
+        io.write("\n\n")
+
+    if references:
+        for k, v in sorted(references.items(), key=lambda x: x[1]["id"]):
+            io.write(f"{to_superscript(v['id'])} {k} - {v['title']}\n")
+            for val in sorted(v["cited_texts"]):
+                io.write(f'   "{val}"\n')
+        io.write("\n")
+
+    stop_reason = message.get("stop_reason", "end_turn")
+    if stop_reason != "end_turn":
+        io.write(f"Response ended prematurely. **Stop reason:** {stop_reason}\n\n")
+
+
 def history_to_string(history: list[dict], pretty: bool, wrap_width: int | None = None) -> str:
     io = StringIO()
     for message in history:
         if message["role"] == "system":
-            io.write("\n# System prompt\n")
-            for content_block in message["content"]:
-                if content_block.type == "text":
-                    io.write(f"{content_block.text}\n")
+            write_system_message(message, io)
         elif message["role"] == "user":
-            prompt = f"{common.CHEVRON} "
-            input = message["content"][0]["text"]
-            if pretty:
-                prompt = common.prompt_style(prompt)
-                input = common.input_style(input)
-
-            io.write(f"{prompt}{input}\n")
+            write_user_message(message, io, pretty)
         elif message["role"] == "assistant":
-            references = {}
-
-            block_type = None
-            for content_block in message["content"]:
-                if block_type is not None and block_type != content_block["type"]:
-                    io.write(f"\n\n")
-
-                block_type = content_block.get("type")
-
-                if block_type == "thinking":
-                    io.write(f"```thinking\n{content_block['thinking']}\n```")
-                elif block_type == "text":
-                    io.write(f"{content_block['text']}")
-                elif block_type == "server_tool_use":
-                    name = content_block.get("name")
-                    if name == "web_search":
-                        io.write(f"Searching the web for `{content_block.get("input", {}).get("query", "<unknown>")}`.")
-                    elif name == "code_execution":
-                        # It's always Python, see https://docs.anthropic.com/en/docs/agents-and-tools/tool-use/code-execution-tool
-                        io.write(f"Running the following Python code:\n\n")
-                        io.write(f"```python\n{content_block.get("input", {}).get("code", "<unknown>")}\n```")
-                    else:
-                        io.write(f"Unknown tool: {name}")
-                elif block_type == "web_search_tool_result":
-                    io.write(f"Found {len(content_block.get("content", []))} results.")
-                elif block_type == "code_execution_tool_result":
-                    output = content_block.get("content", {})
-
-                    if output:
-                        return_code = output.get("return_code", 0)
-                        if return_code != 0:
-                            io.write(f"Code execution failed with return code {return_code}.")
-
-                        stdout = output.get("stdout", "")
-                        if stdout:
-                            io.write(f"```stdout\n{stdout.strip()}\n```")
-
-                        stderr = output.get("stderr", "")
-                        if stderr:
-                            io.write(f"```stderr\n{stderr.strip()}\n```")
-
-                else:
-                    io.write(f"```{block_type}\n{content_block}\n```")
-
-                if content_block and content_block.get("type") == "text" and content_block.get("citations"):
-                    superscripts = set()
-                    for citation in content_block["citations"]:
-                        if citation.get("type") != "web_search_result_location":
-                            continue
-                        url = citation.get("url")
-                        if url is None:
-                            continue
-
-                        if not url in references:
-                            references[url] = {
-                                "title": citation.get("title", ""),
-                                "cited_texts": set(),
-                                "id": len(references) + 1,
-                            }
-
-                        reference = references[url]
-                        if "cited_text" in citation:
-                            reference["cited_texts"].add(citation["cited_text"])
-
-                        superscripts.add(str(reference["id"]))
-
-                    io.write(f"{to_superscript(','.join(sorted(superscripts)))}")
-
-            if block_type is not None:
-                io.write("\n\n")
-
-            if references:
-                for k, v in sorted(references.items(), key=lambda x: x[1]["id"]):
-                    io.write(f"{to_superscript(v['id'])} {k} - {v['title']}\n")
-                    for val in sorted(v["cited_texts"]):
-                        io.write(f'   "{val}"\n')
-                io.write("\n")
+            write_assistant_message(message, io)
 
     result = io.getvalue()
     if pretty:

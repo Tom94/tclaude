1a5109e00188f6ed118e124e093bbb0c97c8c3de
Thomas Müller <git@tom94.net>
2025-05-26 17:41:31 +0200

    chore: add license headers to all files

--- prompt.py
+++ prompt.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python3
 
+# tai -- Terminal AI
+#
+# Copyright (C) 2025 Thomas Müller <contact@tom94.net>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import aiohttp
 import asyncio
 import importlib
--- tools.py
+++ tools.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python3
 
+# tai -- Terminal AI
+#
+# Copyright (C) 2025 Thomas Müller <contact@tom94.net>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 """
 All functions in this file will be auto-converted to tool calls for Claude. Do not put any function into this file that should not be
 directly callable by Claude.
